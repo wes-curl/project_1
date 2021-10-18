2fecafa0fbff95d1780eb1c3d167f04d096cbba1
Iris <58442094+sleepyStick@users.noreply.github.com>
2021-10-18 15:57:25 -0700

    updated prof schema

--- professor.js
+++ professor.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose')
 
 const professorSchema = new Mongoose.Schema({
-  
-  
+  name: String,
+  reviews: [{
+    rating: Number,
+    comment: String
+  }],
+  rating: Number,
+  count: Number
 }, {collection: TBD})
 
 const prefessor = mongoose.model(TBD, professorSchema)
