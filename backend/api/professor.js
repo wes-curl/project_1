661b89d5b4af3f23247e6b3d6e2604f31da6e0b7
sleepyStick <itsirisho@gmail.com>
2021-10-29 00:49:19 -0700

    should post the review now
    still needs to update the ratings and all that jazzzzz

--- backend/api/professor.js
+++ backend/api/professor.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const professorServices = require('../models/professor-services')
 const courseServices = require('../models/course-services')
-const router = express.Router();
-const app = express();
+const app = express.Router();
 
 app.post('/review', async (req, res) => {
+    console.log("new")
+    const name = req.body.name;
     const course = req.body.course;
     const rating = req.body.rating;
     const term = req.body.term;
@@ -12,10 +13,19 @@ app.post('/review', async (req, res) => {
     const comment = req.body.comment;
     
     // if class doens't exist, create new class and add this prof to that list
+    // if class exists, make sure that prof is known to teach that class
     const professors = await courseServices.findProfByCourse(course)
+    if (professors === null) {
+        courseServices.addCourse(course, name)
+    } else if (professors.filter( (prof) => prof == name) == []) {
+        courseServices.addProfessor(course, name)
+    }
+    
+    // add the review under the prefessor
+    professorServices.addReview(name, course, rating, term, year, comment)
 
 
-    res.send(professors)
+    res.send("added")
 })
 
-module.exports = router;
+module.exports = app;
--- backend/models/professor-services.js
+++ backend/models/professor-services.js
@@ -1,2 +1,22 @@
 const mongoose = require('mongoose');
-const professorModel = require('./professor');
+const Professor = require('./professor');
+
+async function addReview(name, course, rating, term, year, comment) {
+    try {
+        const professor = await Professor.findOne({"name": name})
+        const updatedProfessor = professor.reviews.push({
+                                    'course': course,
+                                    'rating': rating,
+                                    'term': term,
+                                    'year': year,
+                                    'comment': comment
+                                })
+        const savedProfessor = professor.save()
+        return savedProfessor
+    } catch (error) {
+        console.log(error)
+        return false
+    }
+}
+
+exports.addReview = addReview;
