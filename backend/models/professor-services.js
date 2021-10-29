0516c72d8369d2121ed70e660d81287842cd6100
sleepyStick <itsirisho@gmail.com>
2021-10-29 14:50:36 -0700

    uses find one and update function now

--- backend/models/professor-services.js
+++ backend/models/professor-services.js
@@ -3,16 +3,18 @@ const Professor = require('./professor');
 
 async function addReview(name, course, rating, term, year, comment) {
     try {
-        const professor = await Professor.findOne({"name": name})
-        const updatedProfessor = professor.reviews.push({
-                                    'course': course,
-                                    'rating': rating,
-                                    'term': term,
-                                    'year': year,
-                                    'comment': comment
-                                })
-        const savedProfessor = professor.save()
-        return savedProfessor
+        const filter = {"name": name}
+        const newCalcs = await professorRatingUpdate(name, rating)
+        const update = {"avgRating": newCalcs[1],
+                        "numRatings": newCalcs[0],
+                        $push: {'reviews': {
+                            'course': course,
+                            'rating': rating,
+                            'term': term,
+                            'year': year,
+                            'comment': comment
+                        }}}
+        await Professor.findOneAndUpdate(filter, update)
     } catch (error) {
         console.log(error)
         return false
