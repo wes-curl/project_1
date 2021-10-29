8bb5f4034089b99cdc7653103dba5cdd02a651cc
Arshia Prince Shah <arshia.shahrodizadh@gmail.com>
2021-10-29 14:42:49 -0700

    added dropdown

---
+++ frontend/src/dropdown.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function DropDown(){
+   return( <select>
+      <option> Professor </option>
+      <option> Class </option>
+      </select>
+   )
+   }
+   export default DropDown;
--- frontend/src/searchBar.js
+++ frontend/src/searchBar.js
@@ -1,10 +1,10 @@
 import React from "react";
+import DropDown from "./dropdown";
 
 function SearchBar(props) {
   return (
     <div className="searchBar">
-      <div className="dropdown"></div>
-      <div className="fillIn"></div>
+      <DropDown />
     </div>
   );
 }
