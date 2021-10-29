d5b6f1b972d347c064d6689099855d81f234f208
sleepyStick <itsirisho@gmail.com>
2021-10-28 21:44:59 -0700

    fixed inconsistencies

--- backend/api/professor.js
+++ backend/api/professor.js
@@ -1,6 +1,21 @@
 const express = require('express');
-// const course = require('../models/course-services')
+const professorServices = require('../models/professor-services')
+const courseServices = require('../models/course-services')
 const router = express.Router();
 const app = express();
+
+app.post('/review', async (req, res) => {
+    const course = req.body.course;
+    const rating = req.body.rating;
+    const term = req.body.term;
+    const year = req.body.year;
+    const comment = req.body.comment;
+    
+    // if class doens't exist, create new class and add this prof to that list
+    const professors = await courseServices.findProfByCourse(course)
+
+
+    res.send(professors)
+})
 
 module.exports = router;
--- backend/models/course-services.js
+++ backend/models/course-services.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const Course = require('./course');
 
-async function getProfessorsByCourse(course_name) {
+async function findProfByCourse(course_name) {
     return await Course.findOne({'name': course_name})
 }
+
+exports.findProfByCourse = findProfByCourse;
