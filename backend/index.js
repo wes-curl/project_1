0b6273b41e0e98fdb71d6855e39a154effc5ffe8
sleepyStick <itsirisho@gmail.com>
2021-10-28 21:16:16 -0700

    took out old code..

--- backend/index.js
+++ backend/index.js
@@ -9,25 +9,6 @@ const port = 5000;
 
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-//     // professors = course_services.getProfessorsByCourse(course)
-//     res.send('Hello World!');
-// });
-
-// app.get('/api/course/getProfessors', (req, res) => {
-//     course = req.body.course;
-//     rating = req.body.rating;
-//     term = req.body.term;
-//     year = req.body.year;
-//     comment = req.body.comment;
-
-//     console.log(course)
-//     res.send(course)
-    
-//     // if class doens't exist, create new class and add this prof to that list
-//     // professors = course_services.getProfessorsByCourse(course)
-//     // res.send(professors)
-// })
 const courseEndpoint = require('./api/course')
 // const professorEndpoint = require('./api/professor.js')
 
