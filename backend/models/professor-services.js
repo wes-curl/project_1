902daa3628b2746e3266b09eb29d5ee39e49e7da
aeller-design <70665075+aeller-design@users.noreply.github.com>
2021-10-29 14:51:37 -0700

    avg rating func added

--- backend/models/professor-services.js
+++ backend/models/professor-services.js
@@ -1,2 +1,16 @@
 const mongoose = require('mongoose');
 const professorModel = require('./professor');
+
+// input - prof object, new rating integer | {0 <= x <= 5}7
+async function professorRatingUpdate(prof, newRating) {
+    // newAvg = ((oldAvg * OldNumRatings) + newRating)/(OldNumRatings + 1)
+    const newNum = (prof.numRatings + 1);
+    const newAvg = ((prof.AvgRating * prof.numRatings) + newRating) / newNum;
+
+    // update professor avg rating and numRatings
+    // const filter = {name: prof.name};
+    // const update = {avgRating : newAvg, numRatings: newNum}
+    // await professor.findOneAndUpdate(filter, update);
+
+
+}
