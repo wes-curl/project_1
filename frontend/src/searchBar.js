8a0189cdbaaf25b08ad5b16208f51a178d8980bd
Arshia Prince Shah <arshia.shahrodizadh@gmail.com>
2021-10-29 14:52:56 -0700

    Completed the bare structure of the main page

--- frontend/src/searchBar.js
+++ frontend/src/searchBar.js
@@ -4,9 +4,28 @@ import DropDown from "./dropdown";
 function SearchBar(props) {
   return (
     <div className="searchBar">
+      <form>
       <DropDown />
+      {searchBar()}
+      {inputSubmit()}
+      </form>
     </div>
   );
 }
 
 export default SearchBar;
+
+function searchBar(){
+  return( <textarea> 
+
+  </textarea> 
+
+  );
+}
+
+function inputSubmit(){
+  return(<input type="submit"> 
+  </input>
+
+  );
+}
