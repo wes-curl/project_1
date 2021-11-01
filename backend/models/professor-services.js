2f599d0562660ab80867bb808bcf1cb4f2f8ecf3
sajanjohal <iceejohal@gmail.com>
2021-11-01 14:37:42 -0700

    Added 2 functions

--- backend/models/professor-services.js
+++ backend/models/professor-services.js
@@ -1,2 +1,22 @@
 const mongoose = require('mongoose');
-const professorModel = require('./professor');
+const Professor = require('./professor');
+
+async function findProfByNameAndDept(professorName, professorDept) {
+
+    return await Professor.findOne({'name': professorName, 'dept': professorDept});
+}
+
+async function addProfessor(professor) {
+
+    try {
+        const professorToAdd = new Professor(professor);
+        const savedProfessor = await professorToAdd.save();
+        return savedProfessor;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+
+exports.findProfByNameAndDept = findProfByNameAndDept;
+exports.addProfessor = addProfessor;
