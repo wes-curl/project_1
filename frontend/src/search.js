9c6707ecba769cc9a083e0f159cd57b1ac82e7b1
Arshia Prince Shah <arshia.shahrodizadh@gmail.com>
2021-11-03 15:58:49 -0700

    Added to homepage

--- frontend/src/search.js
+++ frontend/src/search.js
@@ -2,11 +2,12 @@ import polyLogo from "./polylogo.png";
 import SearchBar from "./searchBar.js";
 import polyDay from "./calpoly.jpg"
 import React from "react";
-import "./search.css";
+import "./homepage.css";
 
 function SearchPage(props) {
   return (
     <div className="searchContainer">
+      <h1> Welcome to PolyRatings! </h1>
       <img className="polyLogo" src={polyLogo} alt=""></img>
       <img className="polyDay" src={polyDay} alt=""></img>
       <SearchBar />
