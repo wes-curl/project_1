4786cc5ab254fa8e7589a1cd268a6a528225d6be
aeller-design <70665075+aeller-design@users.noreply.github.com>
2021-11-03 16:22:48 -0700

    test3

--- backend/models/course-services.js
+++ backend/models/course-services.js
@@ -4,5 +4,5 @@ const Course = require('./course');
 async function findProfByCourse(course_name) {
     return await Course.findOne({'name': course_name})
 }
-//test 2
+//test 3
 exports.findProfByCourse = findProfByCourse;
