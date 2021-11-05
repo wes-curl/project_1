5c4a428479cd3c0ab8f076db9295de5b155fa942
wes-curl <wesleycurl1111@gmail.com>
2021-11-05 09:59:53 -0700

    Set up some basic CSS

--- frontend/src/professorListElement.js
+++ frontend/src/professorListElement.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Professor from "./Professor.js";
 import { Link } from 'react-router-dom';
+import Rating from "./Rating.js";
 
 import styles from "./professorList.module.css";
 
@@ -8,10 +9,10 @@ function ProfessorListElement(props) {
     
     return (
         <div className={styles.professorListElement}>
-            <div className={styles.pprofessorName}>{props.professor.name}</div>
-            <div className={styles.pprofessorRating}>{props.professor.avg_rating}</div>
+            <div className={styles.professorName}>{props.professor.name}</div>
+            <Rating rating={props.professor.avg_rating} numRatings={props.professor.num_ratings}/>
             <Link to="/professor">
-            <input type="button" value="See review" onClick={() => props.onClick(props.professor)} /> 
+            <input className={styles.linkButton} type="button" value="See review" onClick={() => props.onClick(props.professor)} /> 
             </Link>
             <hr/>
         </div>
