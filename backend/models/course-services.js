6f29477a52bacbfdbcee3d6c661f04bd2d14d42c
aeller-design <70665075+aeller-design@users.noreply.github.com>
2021-11-05 15:26:43 -0700

    moved yml file (again)

--- backend/models/course-services.js
+++ backend/models/course-services.js
@@ -4,5 +4,5 @@ const Course = require('./course');
 async function findProfByCourse(course_name) {
     return await Course.findOne({'name': course_name})
 }
-//test 3
+//test 4
 exports.findProfByCourse = findProfByCourse;
