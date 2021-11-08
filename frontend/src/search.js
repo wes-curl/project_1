66f89058628616e6bc60827f8126d423b27e0329
wes-curl <wesleycurl1111@gmail.com>
2021-11-08 15:27:04 -0800

    fixed merge error

--- frontend/src/search.js
+++ frontend/src/search.js
@@ -2,9 +2,9 @@ import polyLogo from "./polylogo.png";
 import SearchBar from "./searchBar.js";
 import polyDay from "./calpoly.jpg"
 import React from "react";
+import styles from "./homepage.module.css";
 
 function SearchPage(props) {
-  import("./search.css");
   return (
     <div className= {styles.searchContainer}>
       <h1> Welcome to PolyRatings! </h1>
