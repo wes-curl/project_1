cf661e984fe182389a7edf49d4402735299f3ede
sajanjohal <iceejohal@gmail.com>
2021-11-10 12:57:41 -0800

    Added '/' GET route

--- backend/api/professor.js
+++ backend/api/professor.js
@@ -1,39 +1,49 @@
-const express = require('express');
-const professorServices = require('../models/professor-services')
-const courseServices = require('../models/course-services')
+const express = require("express");
+const professorServices = require("../models/professor-services");
+const courseServices = require("../models/course-services");
 const app = express.Router();
 
-app.post('/review', async (req, res) => {
-    const course = req.body.course;
-    const rating = req.body.rating;
-    const term = req.body.term;
-    const year = req.body.year;
-    const comment = req.body.comment;
-    
-    // if class doens't exist, create new class and add this prof to that list
-    const professors = await courseServices.findProfByCourse(course)
+app.get("/", async (req, res) => {
+  const professorName = req.body.name;
+  const professorDept = req.body.dept;
 
+  const professors = await professorServices.findProfessor(
+    professorName,
+    professorDept
+  );
 
-    res.send(professors)
-})
+  res.send(professors);
+});
 
-app.post('/', async (req, res) => {
-    
-    const professor = req.body;
-    professor.active = true;
+app.post("/review", async (req, res) => {
+  const course = req.body.course;
+  const rating = req.body.rating;
+  const term = req.body.term;
+  const year = req.body.year;
+  const comment = req.body.comment;
 
-    const existingProfessor = await professorServices.findProfByNameAndDept(professor.name, professor.dept);
+  // if class doens't exist, create new class and add this prof to that list
+  const professors = await courseServices.findProfByCourse(course);
 
-    if (existingProfessor == undefined || existingProfessor == null) {
+  res.send(professors);
+});
 
-        const newProfessor = await professorServices.addProfessor(professor);
+app.post("/", async (req, res) => {
+  const professor = req.body;
+  professor.active = true;
 
-        if (newProfessor)
-            res.status(201).send(newProfessor);
-        else
-            res.status(500).end();
-    }
-    res.status(202).end();  // Professor already exists in DB
+  const existingProfessor = await professorServices.findProfessor(
+    professor.name,
+    professor.dept
+  );
+
+  if (existingProfessor == undefined || existingProfessor == null) {
+    const newProfessor = await professorServices.addProfessor(professor);
+
+    if (newProfessor) res.status(201).send(newProfessor);
+    else res.status(500).end();
+  }
+  res.status(202).end(); // Professor already exists in DB
 });
 
 module.exports = app;
