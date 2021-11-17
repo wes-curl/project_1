71dbd5b0fa73d77d56b91044a5e18a6dadd91202
wes-curl <wesleycurl1111@gmail.com>
2021-11-17 15:39:04 -0800

    Ratings set up

--- frontend/src/ratingEntry.js
+++ frontend/src/ratingEntry.js
@@ -3,10 +3,13 @@ import React from 'react';
 import styles from "./review.module.css";
 
 function RatingEntry(props){
+    function updateSibling(){
+        document.getElementById("ratingValueDisplay").innerHTML = document.getElementById("ratingValue").value
+    }
     return(
         <div className={styles.ratingEntry}>
             <label className={styles.ratingLabel}>Enter a rating: </label>
-            <input className={styles.ratingSlider} type="range" id="ratingValue" name="rating" min="0" max="5" step="0.5"></input>
+            <input className={styles.ratingSlider} type="range" id="ratingValue" name="rating" min="0" max="5" step="0.25" onInput={updateSibling}></input>
             <label id="ratingValueDisplay" className={styles.ratingValueDisplay}>5.0</label>
         </div>
     );
