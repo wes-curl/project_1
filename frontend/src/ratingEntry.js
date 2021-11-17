d995428cecd8e2e72889e9ebb88fdb565dba59a5
wes-curl <wesleycurl1111@gmail.com>
2021-11-17 15:31:08 -0800

    Fixed formatting

--- frontend/src/ratingEntry.js
+++ frontend/src/ratingEntry.js
@@ -6,8 +6,8 @@ function RatingEntry(props){
     return(
         <div className={styles.ratingEntry}>
             <label className={styles.ratingLabel}>Enter a rating: </label>
-            <input className={styles.ratingSlider} type="range" id="ratingValue" name="rating" min="0" max="5" step="0.1"></input>
-            <label id="ratingValueDisplay"></label>
+            <input className={styles.ratingSlider} type="range" id="ratingValue" name="rating" min="0" max="5" step="0.5"></input>
+            <label id="ratingValueDisplay" className={styles.ratingValueDisplay}>5.0</label>
         </div>
     );
 }
